2989cec1bec97a60f721045131531234b5384a73
Naim Tahir Baig <Authordrnaim@gmail.com>
2026-09-25 11:56:57 +0500

    Add role-specific bookmarkable login links

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   images: {
     domains: ['localhost'],
   },
+  async rewrites() {
+    return [
+      { source: "/case-register", destination: "/case-register.html" },
+      { source: "/invigilator", destination: "/case-register.html?role=invigilator" },
+      { source: "/coordinator", destination: "/case-register.html?role=coordinator" },
+      { source: "/senior", destination: "/case-register.html?role=senior" }
+    ];
+  },
 }
 
 module.exports = nextConfig
+
